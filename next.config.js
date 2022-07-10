12fafa5431d0d61206d58fa7d181873a63d694f6
Felipe <felipeeperez3@gmail.com>
2022-07-10 03:13:13 -0400

    V1.0.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress:true,
+  eslint:{
+    ignoreDuringBuilds:true
+  },
+  images:{
+    domains:[
+      "tailwindui.com",
+      "ui-avatars.com",
+      "api.lorem.space",
+
+    ]
+  }
 }
 
 module.exports = nextConfig
